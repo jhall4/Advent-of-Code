68cc3935d8bffa8144167867a866c56dff42f6aa
jhall4 <jhall4@gmail.com>
2021-12-11 18:16:07 -0500

    day eleven

--- Tests/test_DataImporter.py
+++ Tests/test_DataImporter.py
@@ -200,3 +200,20 @@ def test_GetAs2dArray_FileExistsAndHasData_CorrectArrayReturned():
     fileName = "TestData/daynine.csv"
     array = DataImporter.GetAs2dArray(fileName)
     assert (array == expectedArray)
+
+def test_GetAs2dArray_DayElevenFile_CorrectArrayReturned():
+    expectedArray = testData = [
+        [5,4,8,3,1,4,3,2,2,3],
+        [2,7,4,5,8,5,4,7,1,1],
+        [5,2,6,4,5,5,6,1,7,3],
+        [6,1,4,1,3,3,6,1,4,6],
+        [6,3,5,7,3,8,5,4,7,8],
+        [4,1,6,7,5,2,4,6,4,5],
+        [2,1,7,6,8,4,1,7,2,1],
+        [6,8,8,2,8,8,1,1,3,4],
+        [4,8,4,6,8,4,8,5,5,4],
+        [5,2,8,3,7,5,1,5,2,6]
+    ]
+    fileName = "TestData/dayeleven.csv"
+    array = DataImporter.GetAs2dArray(fileName)
+    assert (array == expectedArray)
