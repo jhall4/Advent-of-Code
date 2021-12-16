679b627889808d3b95738554861c841509d8c009
jhall4 <jhall4@gmail.com>
2021-12-15 23:35:24 -0500

    day fifteen

--- Tests/test_DataImporter.py
+++ Tests/test_DataImporter.py
@@ -232,6 +232,24 @@ def test_GetAs2dArray_DayElevenFile_CorrectArrayReturned():
     assert (array == expectedArray)
 
 
+def test_GetAs2dArray_DayFifteenFile_CorrectArrayReturned():
+    expectedArray = [
+        [1,1,6,3,7,5,1,7,4,2],
+        [1,3,8,1,3,7,3,6,7,2],
+        [2,1,3,6,5,1,1,3,2,8],
+        [3,6,9,4,9,3,1,5,6,9],
+        [7,4,6,3,4,1,7,1,1,1],
+        [1,3,1,9,1,2,8,1,3,7],
+        [1,3,5,9,9,1,2,4,2,1],
+        [3,1,2,5,4,2,1,6,3,9],
+        [1,2,9,3,1,3,8,5,2,1],
+        [2,3,1,1,9,4,4,5,8,1]
+    ]
+    fileName = "TestData/dayfifteen.csv"
+    array = DataImporter.GetAs2dArray(fileName)
+    assert (array == expectedArray)
+
+
     ##### GetAsPointsAndFolds #####
 
 
